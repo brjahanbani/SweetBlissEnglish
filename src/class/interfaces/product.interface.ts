7bcf236a5012da68dec59c07c03c84d4a16be5f9
Babak <babak.jahanbani@yahoo.com>
2022-05-16 20:36:30 +0430

    Product Class & Interface

---
+++ src/class/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+export interface ProductInterface {
+  id: number;
+  categoryId: number;
+  name: string;
+  price: number;
+  code: string;
+  description: string;
+}
--- src/class/product.class.ts
+++ src/class/product.class.ts
@@ -1,8 +1,18 @@
-export class Product {
-  id!: number;
-  categoryId!: number;
-  name!: string;
-  price!: number;
-  code!: string;
-  description!: string;
+import { ProductInterface } from './interfaces/product.interface';
+export class Product implements ProductInterface {
+  public id!: number;
+  public categoryId!: number;
+  public name!: string;
+  public price!: number;
+  public code!: string;
+  public description!: string;
+
+  constructor(data: Product) {
+    this.id = data.id;
+    this.categoryId = data.categoryId;
+    this.name = data.name;
+    this.price = data.price;
+    this.code = data.code;
+    this.description = data.description;
+  }
 }
