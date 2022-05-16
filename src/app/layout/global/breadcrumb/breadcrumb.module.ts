e4161207e7b234a83011fd26f4c3e3b8e98cfee2
Babak <babak.jahanbani@yahoo.com>
2022-05-16 13:12:42 +0430

    Breadcrumb initial appearance & import Categories

--- src/app/layout/global/breadcrumb/breadcrumb.module.ts
+++ src/app/layout/global/breadcrumb/breadcrumb.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from './breadcrumb.component';
 
-
-
 @NgModule({
-  declarations: [
-    BreadcrumbComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [BreadcrumbComponent],
+  imports: [CommonModule],
+  exports: [BreadcrumbComponent],
 })
-export class BreadcrumbModule { }
+export class BreadcrumbModule {}
--- src/app/layout/pages/products/categories/categories.module.ts
+++ src/app/layout/pages/products/categories/categories.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
 import { CategoryCardModule } from 'src/app/layout/global/category-card/category-card.module';
+import { BreadcrumbModule } from 'src/app/layout/global/breadcrumb/breadcrumb.module';
 
 @NgModule({
   declarations: [CategoriesComponent],
-  imports: [CommonModule, CategoriesRoutingModule, CategoryCardModule],
+  imports: [
+    CommonModule,
+    CategoriesRoutingModule,
+    CategoryCardModule,
+    BreadcrumbModule,
+  ],
 })
 export class CategoriesModule {}
