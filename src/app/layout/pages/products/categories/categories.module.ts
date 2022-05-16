9137ee310b254f78a9606cfa3577230f3d5f4e41
Babak <babak.jahanbani@yahoo.com>
2022-05-16 13:07:34 +0430

    import category-card to categories module

--- src/app/layout/pages/products/categories/categories.module.ts
+++ src/app/layout/pages/products/categories/categories.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
-
+import { CategoryCardModule } from 'src/app/layout/global/category-card/category-card.module';
 
 @NgModule({
-  declarations: [
-    CategoriesComponent
-  ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+  declarations: [CategoriesComponent],
+  imports: [CommonModule, CategoriesRoutingModule, CategoryCardModule],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
