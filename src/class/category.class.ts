b3ee6661d1796986750e3444f4e9e1ac31de2ef3
Babak <babak.jahanbani@yahoo.com>
2022-05-16 13:05:59 +0430

    Create Category Class & Interface

--- src/class/category.class.ts
+++ src/class/category.class.ts
@@ -1,5 +1,18 @@
-export class Category {
-  id!: number;
-  name!: string;
-  code!: string;
+import { CategoryInteface } from './interfaces/category.interface';
+import { Product } from './product.class';
+
+export class Category implements CategoryInteface {
+  public id!: number;
+  public name!: string;
+  public code!: string;
+  public imageSrc!: string;
+  public products?: Product[];
+
+  constructor(data: Category) {
+    this.id = data.id;
+    this.name = data.name;
+    this.code = data.code;
+    this.imageSrc = data.imageSrc;
+    this.products = data.products;
+  }
 }
---
+++ src/class/interfaces/category.interface.ts
@@ -0,0 +1,6 @@
+export interface CategoryInteface {
+  id: number;
+  name: string;
+  code: string;
+  imageSrc: string;
+}
