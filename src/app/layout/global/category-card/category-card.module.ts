881e3d07e47bed538604d7f979618b7b633e563e
Babak <babak.jahanbani@yahoo.com>
2022-05-16 13:06:48 +0430

    Create Category Card in Global Folder

--- src/app/layout/global/category-card/category-card.module.ts
+++ src/app/layout/global/category-card/category-card.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryCardComponent } from './category-card.component';
 
-
-
 @NgModule({
-  declarations: [
-    CategoryCardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CategoryCardComponent],
+  imports: [CommonModule],
+  exports: [CategoryCardComponent],
 })
-export class CategoryCardModule { }
+export class CategoryCardModule {}
