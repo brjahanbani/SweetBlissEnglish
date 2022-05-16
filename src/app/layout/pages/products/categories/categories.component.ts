35520273f95ea2d1a60c5d0a94a12b6fa265f513
Babak <babak.jahanbani@yahoo.com>
2022-05-16 20:10:48 +0430

    Final setting from CatCard & making CatsPage work

--- src/app/layout/pages/products/categories/categories.component.ts
+++ src/app/layout/pages/products/categories/categories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { exhaustMap, Observable } from 'rxjs';
+import { CategoryService } from 'src/app/services/category.service';
+import { CategoryInterface } from 'src/class/interfaces/category.interface';
 
 @Component({
   selector: 'app-categories',
@@ -6,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-  constructor() {}
+  // categories: any;
+  categories$!: Observable<CategoryInterface[]>;
+  constructor(private categoryService: CategoryService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.categoryService
+    //   .getCategories()
+    //   .subscribe((data: CategoryInteface[]) => {
+    //     this.categories = data;
+    //   });
+    this.categories$ = this.categoryService.getCategories();
+  }
 }
