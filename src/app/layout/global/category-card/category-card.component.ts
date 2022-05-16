3e4a5df6e987d1071f4698224e0f584119253a8f
Babak <babak.jahanbani@yahoo.com>
2022-05-16 20:05:52 +0430

    Creating Final category-card in global

--- src/app/layout/global/category-card/category-card.component.ts
+++ src/app/layout/global/category-card/category-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { CategoryInterface } from 'src/class/interfaces/category.interface';
 
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.css']
+  styleUrls: ['./category-card.component.css'],
 })
 export class CategoryCardComponent implements OnInit {
+  @Input() category!: CategoryInterface;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
