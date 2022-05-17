6b5e85a38e4250404955e16abe877d4847c78a3b
Babak <babak.jahanbani@yahoo.com>
2022-05-17 13:00:17 +0430

    Important Change in getCategories&Create getCategory

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { Category } from 'src/class/category.class';
+import { Product } from 'src/class/product.class';
 import { CategoryInterface } from 'src/class/interfaces/category.interface';
 
 // import GetCategoriesJson from 'src/mock/getCategories.json';
@@ -12,14 +14,34 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
   //   private categories: CategoryInterface[] = GetCategoriesJson;
 
-  getCategories(): Observable<CategoryInterface[]> {
+  getCategories(): Observable<Category[]> {
     // return of(this.categories).pipe(shareReplay());
-    return this.http.get<CategoryInterface[]>('assets/mock/getCategories.json');
-    //   .pipe(map((response) => response as CategoryInterface[]));
+    return this.http
+      .get<CategoryInterface[]>('assets/mock/getCategories.json')
+      .pipe(
+        map((categoryInterfaceArray: CategoryInterface[]) =>
+          categoryInterfaceArray.map(
+            (categoryInterface) =>
+              new Category({ ...categoryInterface, products: undefined })
+          )
+        )
+      );
   }
 
-  //   getCategory(id: number): Observable<CategoryInterface | undefined> {
-  //     const category = this.categories.find((e) => e.id == id);
-  //     return of(category);
-  //   }
+  getCategory(id: number): Observable<Category> {
+    // const category = this.categories.find((e) => e.id == id);
+    // return of(category);
+    console.log('id', id);
+
+    return this.http
+      .get<CategoryInterface>('assets/mock/getCategoryById.json')
+      .pipe(
+        map((categoryInterface: CategoryInterface) => {
+          const products = categoryInterface.products?.map(
+            (productInterface) => new Product(productInterface)
+          );
+          return new Category({ ...categoryInterface, products });
+        })
+      );
+  }
 }
