1361c601a38046318d18949fd6bd4d563c761826
Babak <babak.jahanbani@yahoo.com>
2022-05-18 12:31:27 +0430

    Category Component Finalizing

--- src/app/layout/pages/products/category/category-routing.module.ts
+++ src/app/layout/pages/products/category/category-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './category.component';
 
-const routes: Routes = [{ path: 'category', component: CategoryComponent }];
+const routes: Routes = [
+  {
+    path: 'category/:id',
+    component: CategoryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/layout/pages/products/category/category.component.ts
+++ src/app/layout/pages/products/category/category.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { exhaustMap, map, mergeMap, Observable, tap } from 'rxjs';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/class/category.class';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  category$!: Observable<Category>;
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    // this.category$ = this.categoryService.getCategory();
+    this.category$ = this.route.params.pipe(
+      exhaustMap(({ id }) => this.categoryService.getCategory(id))
+    );
   }
-
 }
