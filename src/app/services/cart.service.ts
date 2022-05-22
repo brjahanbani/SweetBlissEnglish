8c6f2917a93deacf0746599ba75730e1347c4e22
Babak <babak.jahanbani@yahoo.com>
2022-05-22 13:14:01 +0430

    BehavioralSubject vs Observable + cart icon header

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { OrderProduct } from 'src/class/order-product.class';
 import { Product } from 'src/class/product.class';
 
@@ -6,27 +7,31 @@ import { Product } from 'src/class/product.class';
   providedIn: 'root',
 })
 export class CartService {
-  orderProducts: OrderProduct[] = [];
+  private _orderProducts$ = new BehaviorSubject<OrderProduct[]>([]);
 
   constructor() {}
 
+  get orderProducts$(): Observable<OrderProduct[]> {
+    return this._orderProducts$.asObservable();
+  }
+
   get totalPrice(): number {
-    return this.orderProducts
+    return this._orderProducts$.value
       .map((orderProduct) => orderProduct.price)
       .reduce((sum, current) => sum + current, 0);
   }
 
   addToCart(product: Product, quantity: number): void {
-    const index = this.orderProducts
+    const index = this._orderProducts$.value
       .map((orderProduct) => orderProduct.product.id)
       .indexOf(product.id);
     if (index !== -1) {
-      this.orderProducts[index].quantity =
-        quantity + this.orderProducts[index].quantity;
+      this._orderProducts$.value[index].quantity =
+        quantity + this._orderProducts$.value[index].quantity;
     } else {
       const orderProduct = new OrderProduct({ product, quantity });
-      this.orderProducts.push(orderProduct);
+      this._orderProducts$.value.push(orderProduct);
     }
-    console.log(this.orderProducts);
+    console.log(this._orderProducts$.value);
   }
 }
