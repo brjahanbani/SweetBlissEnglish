54a218cb85cab9fc2fe39bf9c52c57bd1849ada6
Babak <babak.jahanbani@yahoo.com>
2022-05-22 18:13:05 +0430

    Progress final touch

--- src/app/layout/global/progress/progress.component.ts
+++ src/app/layout/global/progress/progress.component.ts
@@ -1,52 +1,15 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewEncapsulation,
-} from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 
-export interface Step {
-  label: string;
-  stepId: number;
-  route?: string;
-  disabled: boolean;
-  queryParams: {
-    id?: number;
-    step?: number;
-  };
-}
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class ProgressComponent implements OnInit {
-  @Input() public totalSteps!: Step[]; // chanta step has kolan
-  @Input() public currentStepId!: number; // id e stepe feli k faale
-  @Input() public maxIdEnabled!: number; // bozorg tarin stepi ke mitavanad faal bashad
-  @Input() public disabledBtns = false;
-  @Output() public StepChangeEvent = new EventEmitter();
-  @Input() draftId: any;
-  @Input() isPrimary: any;
-  constructor(public router: Router) {}
+  @Input() currentStep: 1 | 2 | 3 | 4 = 1;
+  @Input() steps = [2, 3, 4];
+
+  constructor() {}
 
   ngOnInit(): void {}
-
-  changeRoute(step: { stepId: number }) {
-    const navigationExtras: NavigationExtras = {
-      queryParams: {
-        step: step.stepId,
-        draftId: this.draftId,
-      },
-    };
-    this.currentStepId = step.stepId;
-    this.StepChangeEvent.emit(step.stepId);
-    console.log('object :>> ', this.router.url);
-    const route = this.router.url.split('?')[0];
-    this.router.navigate([route], navigationExtras);
-  }
 }
