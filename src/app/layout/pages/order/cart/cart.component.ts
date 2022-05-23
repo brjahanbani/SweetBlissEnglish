5a456cece7b3fe94602e63e5ca02b7c87dd3a375
Babak <babak.jahanbani@yahoo.com>
2022-05-23 18:32:54 +0430

    Plus function for cart itself

--- src/app/layout/pages/order/cart/cart.component.ts
+++ src/app/layout/pages/order/cart/cart.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
+import { OrderProduct } from 'src/class/order-product.class';
+import { Product } from 'src/class/product.class';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  orderProducts$!: Observable<OrderProduct[]>;
+  totalPrice!: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private cart: CartService) {
+    this.orderProducts$ = this.cart.orderProducts$;
+    this.totalPrice = this.cart.totalPrice;
   }
 
+  ngOnInit(): void {}
+
+  addToCart(product: Product, quantity: number) {
+    this.cart.addToCart(product, quantity);
+  }
 }
