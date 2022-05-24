f10733e26d6ad564e487a5e11ce17f6c008e8a5c
Babak <babak.jahanbani@yahoo.com>
2022-05-24 12:20:32 +0430

    Initial Shipping Page

--- src/app/layout/pages/order/shipping/shipping.module.ts
+++ src/app/layout/pages/order/shipping/shipping.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ShippingRoutingModule } from './shipping-routing.module';
 import { ShippingComponent } from './shipping.component';
-
+import { ProgressModule } from 'src/app/layout/global/progress/progress.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ShippingComponent
-  ],
-  imports: [
-    CommonModule,
-    ShippingRoutingModule
-  ]
+  declarations: [ShippingComponent],
+  imports: [CommonModule, ShippingRoutingModule, ProgressModule, SharedModule],
 })
-export class ShippingModule { }
+export class ShippingModule {}
