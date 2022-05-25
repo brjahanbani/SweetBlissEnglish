807931a0ba1e79187718c63c5484938e1c7fa78b
Babak <babak.jahanbani@yahoo.com>
2022-05-25 17:53:46 +0430

    Initial Invoice UI

--- src/app/layout/pages/order/invoice/invoice.module.ts
+++ src/app/layout/pages/order/invoice/invoice.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { InvoiceRoutingModule } from './invoice-routing.module';
 import { InvoiceComponent } from './invoice.component';
-
+import { ProgressModule } from 'src/app/layout/global/progress/progress.module';
 
 @NgModule({
-  declarations: [
-    InvoiceComponent
-  ],
-  imports: [
-    CommonModule,
-    InvoiceRoutingModule
-  ]
+  declarations: [InvoiceComponent],
+  imports: [CommonModule, InvoiceRoutingModule, ProgressModule],
 })
-export class InvoiceModule { }
+export class InvoiceModule {}
