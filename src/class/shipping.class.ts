d72825fbad0e1ce9eb817611af10047e56cfd94b
Babak <babak.jahanbani@yahoo.com>
2022-05-25 13:18:08 +0430

    Shipping Calss is deleted & supposed to be string

--- src/class/shipping.class.ts
+++
@@ -1,9 +0,0 @@
-export class Shipping {
-  public id!: number;
-  public address!: string;
-
-  constructor(data: Partial<Shipping>) {
-    this.id = data.id!;
-    this.address = data.address!;
-  }
-}
