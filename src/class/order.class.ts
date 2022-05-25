8b8854da0a0419d2067b0394f1c0adc759c3079d
Babak <babak.jahanbani@yahoo.com>
2022-05-25 17:03:05 +0430

    order class Correction

--- src/class/order.class.ts
+++ src/class/order.class.ts
@@ -1,8 +1,21 @@
+import { OrderProduct } from './order-product.class';
+
 export class Order {
-  id!: number;
-  createdAt!: Date;
-  CustomerId!: number;
-  customerName!: string;
-  shippingId!: number;
-  totalPrice!: number;
+  public id!: number;
+  public createdAt!: Date;
+  public customerId?: number;
+  public customerName!: string;
+  public shippingAddress!: string;
+  public totalPrice!: number;
+  public orderProduct!: OrderProduct;
+
+  constructor(data: Partial<Order>) {
+    this.id = data.id!;
+    this.createdAt = data.createdAt!;
+    this.customerId = data?.customerId;
+    this.customerName = data.customerName!;
+    this.shippingAddress = data.shippingAddress!;
+    this.totalPrice = data.totalPrice!;
+    this.orderProduct = data.orderProduct!;
+  }
 }
