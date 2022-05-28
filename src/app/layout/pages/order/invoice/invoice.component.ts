93a049618927c8b4eff3315ac5c9e9bdeb15ce99
Babak <babak.jahanbani@yahoo.com>
2022-05-28 20:42:31 +0430

    Invoice Page Completion

--- src/app/layout/pages/order/invoice/invoice.component.ts
+++ src/app/layout/pages/order/invoice/invoice.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
+import { ShippingService } from 'src/app/services/shipping.service';
+import { OrderProduct } from 'src/class/order-product.class';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  styleUrls: ['./invoice.component.css'],
 })
 export class InvoiceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  orderProducts$!: Observable<OrderProduct[]>;
+  totalPrice!: number;
+  currentDate = new Date();
+  constructor(
+    private cart: CartService,
+    public shippingService: ShippingService,
+    public authService: AuthService
+  ) {
+    this.orderProducts$ = this.cart.orderProducts$;
+    this.totalPrice = this.cart.totalPrice;
   }
 
+  ngOnInit(): void {}
 }
--- src/app/layout/pages/order/invoice/invoice.module.ts
+++ src/app/layout/pages/order/invoice/invoice.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { InvoiceRoutingModule } from './invoice-routing.module';
 import { InvoiceComponent } from './invoice.component';
