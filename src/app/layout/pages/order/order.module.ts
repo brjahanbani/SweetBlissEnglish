b37d54637f82bb0ebb326644372af5fd4ec5a564
Babak <babak.jahanbani@yahoo.com>
2022-05-30 13:57:43 +0430

    order module correction

--- src/app/layout/pages/order/order.module.ts
+++ src/app/layout/pages/order/order.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OrderRoutingModule } from './order-routing.module';
-import { ProgressModule } from '../../global/progress/progress.module';
 
 @NgModule({
   declarations: [],
