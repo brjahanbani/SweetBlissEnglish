527d28c41af0a26f315fd162eb95efde0df8fa0c
Babak <babak.jahanbani@yahoo.com>
2022-06-02 21:10:35 +0430

    nothing important

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LayoutRoutingModule } from './layout-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './layout.component';
-import { ProgressComponent } from './global/progress/progress.component';
 import { AuthenticationModule } from './pages/authentication/authentication.module';
 
 @NgModule({
